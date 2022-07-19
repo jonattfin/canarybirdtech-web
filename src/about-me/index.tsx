6189a2f22cca6192554c8be73c76607cce2371ca
jonattfin <jonattfin@outlook.com>
2022-07-19 16:39:44 +0300

    added about me details

--- src/about-me/index.tsx
+++ src/about-me/index.tsx
@@ -10,6 +10,20 @@ export default () => {
         spacing={2}
       >
         <Item>
+          <h3>
+            I'm a software engineer passionate about technology and data
+            science.
+          </h3>
+          <p>
+            I have experience in .NET Core (on prem, cloud (Azure)) and
+            Javascript (React / Node).
+          </p>
+          <p>
+            My interests are: product development, sustainable software architecture, open source,
+            serverless computing, IoT and data science, etc.
+          </p>
+        </Item>
+        <Item>
           <h2>Tech Stack</h2>
           <Bump.CustomBump />
         </Item>
